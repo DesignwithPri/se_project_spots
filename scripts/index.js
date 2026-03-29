568b37d26384b4025713a29e7c0619d0acaac976
Hannah Laguerre <laguerrehannah@gmail.com>
2026-03-29 01:56:09 -0400

    updated project

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,32 @@
 const initialCards = [
   {
     name: "Val Thorens",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/places/val-thorens.jpg",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
   },
   {
     name: "Restaurant terrace",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/places/restaurant-terrace.jpg",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
   },
   {
     name: "An outdoor cafe",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/places/outdoor-cafe.jpg",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
   },
   {
     name: "A very long bridge",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/places/long-bridge.jpg",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
   },
   {
     name: "Tunnel with morning light",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/places/tunnel.jpg",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
   },
   {
     name: "Mountain house",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/places/mountain-house.jpg",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
   },
+  {
+    name: "Landscape",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/7-photo-by-griffin-wooldridge-from-pexels.jpg",
+  }
 ];
 
 const editProfileBtn = document.querySelector(".profile__edit-button");
@@ -30,6 +34,11 @@ const newPostBtn = document.querySelector(".profile__add-button");
 
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const newPostModal = document.querySelector("#new-post-modal");
+
+const previewModal = document.querySelector("#preview-modal");
+const previewModalImage = previewModal.querySelector(".modal__image");
+const previewModalCaption = previewModal.querySelector(".modal__caption");
+const previewModalCloseBtn = previewModal.querySelector(".modal__close-btn");
 
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
@@ -47,6 +56,7 @@ const cardCaptionInput = document.querySelector("#card-caption-input");
 const cardImageInput = document.querySelector("#card-image-input");
 
 const cardsList = document.querySelector(".cards__list");
+const cardTemplate = document.querySelector("#card-template");
 
 function openModal(modal) {
   modal.classList.add("modal_is-opened");
@@ -54,6 +64,33 @@ function openModal(modal) {
 
 function closeModal(modal) {
   modal.classList.remove("modal_is-opened");
+}
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.content
+    .querySelector(".card")
+    .cloneNode(true);
+
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const likeButton = cardElement.querySelector(".card__like-button");
+
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+  cardTitle.textContent = data.name;
+
+  likeButton.addEventListener("click", function () {
+    likeButton.classList.toggle("card__like-button_liked");
+  });
+
+  cardImage.addEventListener("click", function () {
+    previewModalImage.src = data.link;
+    previewModalImage.alt = data.name;
+    previewModalCaption.textContent = data.name;
+    openModal(previewModal);
+  });
+
+  return cardElement;
 }
 
 editProfileBtn.addEventListener("click", function () {
@@ -74,6 +111,10 @@ newPostCloseBtn.addEventListener("click", function () {
   closeModal(newPostModal);
 });
 
+previewModalCloseBtn.addEventListener("click", function () {
+  closeModal(previewModal);
+});
+
 editProfileForm.addEventListener("submit", function (evt) {
   evt.preventDefault();
 
@@ -86,20 +127,12 @@ editProfileForm.addEventListener("submit", function (evt) {
 function handleAddCardSubmit(evt) {
   evt.preventDefault();
 
-  const cardTitle = cardCaptionInput.value;
-  const cardImage = cardImageInput.value;
+  const newCard = {
+    name: cardCaptionInput.value,
+    link: cardImageInput.value,
+  };
 
-  const cardElement = document.createElement("li");
-  cardElement.classList.add("card");
-
-  cardElement.innerHTML = `
-    <img class="card__image" src="${cardImage}" alt="${cardTitle}">
-    <div class="card__content">
-      <h2 class="card__title">${cardTitle}</h2>
-      <button class="card__like-button" type="button" aria-label="Like"></button>
-    </div>
-  `;
-
+  const cardElement = getCardElement(newCard);
   cardsList.prepend(cardElement);
 
   addCardFormElement.reset();
@@ -109,5 +142,6 @@ function handleAddCardSubmit(evt) {
 addCardFormElement.addEventListener("submit", handleAddCardSubmit);
 
 initialCards.forEach(function (card) {
-  console.log(card.name);
+  const cardElement = getCardElement(card);
+  cardsList.prepend(cardElement);
 });
